b1b6478a73e8e20b6ca70a1e3a98c2b891858ffc
Artem_Krol <artemkrol47@gmail.com>
2021-07-22 02:07:09 +0300

    add clock, block, transaction  words

--- contracts/keyWords.sol
+++ contracts/keyWords.sol
@@ -5,4 +5,37 @@ contract keyWords{
     uint public oneSzabo = 1 szabo;
     uint public oneFinney = 1 finney;
     uint public oneEther = 1 ether;
+
+    //time
+    uint public oneSecond = 1 seconds;
+    uint public oneMinute = 1 minutes;
+    uint public oneHour = 1 hours;
+    uint public oneWeek = 1 weeks;
+
+    // block
+    bytes32 public blockHash = blockhash(52);
+    address public miner = block.coinbase;
+    uint public difficulty = block.difficulty;
+    uint public gasLimit = block.gaslimit;
+    uint public time = block.timestamp;
+    uint public time2 = now;
+
+    //transaction
+
+    bytes public data = msg.data;
+    uint public gas = gasleft();
+    address public sender = msg.sender;
+    bytes4 public signature = msg.sig;
+    uint public value = msg.value;
+
+
+
+
+
+
+
+
+
+
+
 }
