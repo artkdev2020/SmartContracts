d66727c01e1cddccf53b30f8af3bf8f75883cd3b
Artem_Krol <artemkrol47@gmail.com>
2021-07-22 11:01:42 +0300

    transfer work

--- contracts/functionContract.sol
+++ contracts/functionContract.sol
@@ -1,2 +1,22 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.5.16
+pragma solidity ^0.5.16;
+
+contract functionContract {
+
+    address payable public owner;
+    uint public age;
+
+    constructor (uint _age) public {
+        owner = msg.sender;
+        age = _age;
+    }
+
+    function() external payable {
+        uint value = msg.value / 2;
+        owner.transfer(value);
+
+
+    }
+
+
+}
